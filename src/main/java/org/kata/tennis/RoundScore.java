74973fda8c28705cdeb2ac66db0b726c30f65893
Faisal HOTAK <55158944+faisalhotak@users.noreply.github.com>
2024-01-16 23:13:52 +0100

    refactoring: updated RoundScore class into a Record to improve readability, it was basically storing players scores

--- src/main/java/org/kata/tennis/RoundScore.java
+++ src/main/java/org/kata/tennis/RoundScore.java
@@ -1,33 +1,5 @@
 package org.kata.tennis;
 
-public class RoundScore {
+public record RoundScore(int playerOneScore, int playerTwoScore) {
 
-    private int playerOneScore;
-    private int playerTwoScore;
-
-    public RoundScore() {
-        this.playerOneScore = 0;
-        this.playerTwoScore = 0;
-    }
-
-    public RoundScore(int playerOneScore, int playerTwoScore) {
-        this.playerOneScore = playerOneScore;
-        this.playerTwoScore = playerTwoScore;
-    }
-
-    public int getPlayerOneScore() {
-        return playerOneScore;
-    }
-
-    public int getPlayerTwoScore() {
-        return playerTwoScore;
-    }
-
-    public void playerOneWinsTheRound() {
-        this.playerOneScore++;
-    }
-
-    public void playerTwoWinsTheRound() {
-        this.playerTwoScore++;
-    }
 }
